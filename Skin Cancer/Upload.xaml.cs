230db897e53b740c58ff5e296de624b4f024e327
kbates13 <54421182+kbates13@users.noreply.github.com>
2021-12-16 09:48:30 -0500

    Update Upload.xaml.cs

--- Skin Cancer/Upload.xaml.cs
+++ Skin Cancer/Upload.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,6 +8,11 @@ using System.Text;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using RestSharp;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Skin_Cancer
 {
@@ -17,9 +22,10 @@ namespace Skin_Cancer
         public Upload()
         {
             InitializeComponent();
+
         }
 
-        async void Button_Clicked(object sender, EventArgs e)
+        public async void Button_Clicked(object sender, EventArgs e)
         {
             var photo = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
             {
@@ -38,12 +44,34 @@ namespace Skin_Cancer
                 var request = new RestRequest(Method.POST);
                 request.AddFile("image", newFile);
                 IRestResponse response = client.Execute(request);
+
+                var diffs = JsonConvert.DeserializeObject<Dictionary<string, int>>(response.Content);
+                var maxKeyOnValue = diffs.Where(c => c.Value.Equals("val1")).Select(x => x.Key).Max();
+                var maxK = diffs.FirstOrDefault(x => x.Value == diffs.Values.Max()).Key;
+
+                Dictionary<string, string> com = new Dictionary<string, string>();
+                com.Add("akiec", "Actinic Keratoses and Intraepithelial Carcinoma / Bowen's Disease");
+                com.Add("bcc", "Basal Cell Carcinoma");
+                com.Add("blk", "Benign Keratosis-Like Lesions");
+                com.Add("df", "Dermatofibroma");
+                com.Add("mel", "Melanoma");
+                com.Add("nv", "Melanocytic Nevi");
+                com.Add("vasc", "Vascular");
+
+                string diagnosis = com[maxK];
+
+                string report = String.Format(@"Our image processing model has determined that you case is closest to the {0} skin cancer type. It is important to understand that all diagnosis may not be exact. It is vital to consult with a doctor about any worries of possible skin cancer cases.To further understand the symptoms and general information about your disgnosis, visit the Confirmed Cases tab.", diagnosis);
+
+                Application.Current.MainPage.DisplayAlert("Report", report, "Cancel");
+
                 Console.WriteLine("test\n");
-                Console.WriteLine(response.Content);
+                Console.WriteLine(report);
+
+
             }
         }
 
-        async void Button_Clicked_1(object sender, EventArgs e)
+        public async void Button_Clicked_1(object sender, EventArgs e)
         {
             var photo = await MediaPicker.CapturePhotoAsync();
 
@@ -60,8 +88,34 @@ namespace Skin_Cancer
                 var request = new RestRequest(Method.POST);
                 request.AddFile("image", newFile);
                 IRestResponse response = client.Execute(request);
+
+                var diffs = JsonConvert.DeserializeObject<Dictionary<string, int>>(response.Content);
+                var maxKeyOnValue = diffs.Where(c => c.Value.Equals("val1")).Select(x => x.Key).Max();
+                var maxK = diffs.FirstOrDefault(x => x.Value == diffs.Values.Max()).Key;
+
+                Dictionary<string, string> com = new Dictionary<string, string>();
+                com.Add("akiec", "Actinic Keratoses and Intraepithelial Carcinoma / Bowen's Disease");
+                com.Add("bcc", "Basal Cell Carcinoma");
+                com.Add("blk", "Benign Keratosis-Like Lesions");
+                com.Add("df", "Dermatofibroma");
+                com.Add("mel", "Melanoma");
+                com.Add("nv", "Melanocytic Nevi");
+                com.Add("vasc", "Vascular");
+
+                string diagnosis = com[maxK];
+
+
+                string report = String.Format(@"Our image processing model has determined that you case is closest to the {0} skin cancer type. It is important to understand that all diagnosis may not be exact. It is vital to consult with a doctor about any worries of possible skin cancer cases.To further understand the symptoms and general information about your disgnosis, visit the Confirmed Cases tab.", diagnosis);
+
+
+            
+
+                Application.Current.MainPage.DisplayAlert("Report", report, "Cancel");
+
                 Console.WriteLine("test\n");
-                Console.WriteLine(response.Content);
+                Console.WriteLine(report);
+
+                
             }
         }
 
